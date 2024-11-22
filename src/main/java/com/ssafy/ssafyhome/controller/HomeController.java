d4ff05a70567d9dd41a7d4367e06b6a4f6d1ec44
InJoo <jjoonior97@gmail.com>
2024-11-22 15:19:27 +0900

    Feat: swagger 추가

--- src/main/java/com/ssafy/ssafyhome/controller/HomeController.java
+++ src/main/java/com/ssafy/ssafyhome/controller/HomeController.java
@@ -2,6 +2,8 @@ package com.ssafy.ssafyhome.controller;
 
 import com.ssafy.ssafyhome.domain.entity.Dabang;
 import com.ssafy.ssafyhome.service.HomeService;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -13,11 +15,13 @@ import java.util.List;
 @RestController
 @RequestMapping("/home")
 @RequiredArgsConstructor
+@Tag(name = "다방 크롤링 매물 조회 컨트롤러", description = "다방 크롤링 매물 조회")
 public class HomeController {
 
   private final HomeService homeService;
 
   @GetMapping("/dabang")
+  @Operation(summary = "다방 매물 리스트 조회", description = "")
   public List<Dabang> selectDabangHomeList(@RequestParam(defaultValue = "35.2") float wnLat,
                                            @RequestParam(defaultValue = "126.8") float wnLng,
                                            @RequestParam(defaultValue = "35.18") float esLat,
